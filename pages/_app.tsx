d8518bec6cbe6224b641c0a79fdd5fe77408c4ef
Allan Lopez <n16h7hun73r@gmail.com>
2022-03-07 19:57:49 -0600

    :white_check_mark: Add axios package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import Head from "next/dist/next-server/lib/head";
+import React from 'react';
+import Head from 'next/dist/next-server/lib/head';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const MyApp = ({ Component, pageProps }: any) => (
   <React.Fragment>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta charSet='utf-8' />
     </Head>
     <style jsx global>
       {`
@@ -14,7 +17,9 @@ const MyApp = ({ Component, pageProps }: any) => (
         }
       `}
     </style>
-    <Component {...pageProps} />
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
   </React.Fragment>
 );
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import styled from '@emotion/styled';
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { theme } from '../styles/theme';
 
-const Container = styled.div`
-  background-color: ${theme.colors.red};
-`;
+const Container = styled.div``;
 
 function Home() {
+  const { data, isLoading } = useQuery('ditto', () => {
+    return axios.get('https://pokeapi.co/api/v2/pokemon/squirtle');
+  });
+
   return <Container>Hello World!</Container>;
 }
 
