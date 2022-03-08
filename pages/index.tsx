5478d41945c4ce315c6f9bfc1b54176b9f860ccd
Allan Lopez <n16h7hun73r@gmail.com>
2022-03-07 19:12:23 -0600

    :heavy_plus_sign: Fix emotion styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import styled from '@emotion/styled';
 import React from 'react';
+import { theme } from '../styles/theme';
 
-function Home({ test }) {
-  return <div>Hello World!</div>;
+const Container = styled.div`
+  background-color: ${theme.colors.red};
+`;
+
+function Home() {
+  return <Container>Hello World!</Container>;
 }
 
 export default Home;
